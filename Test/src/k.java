1d92b1a5d8a1e2e37dfe2fb4ac96d08d552238fb
VJ <VJ@LAPTOP-P9U4FI6A.fios-router.home>
2019-03-27 05:14:54 -0400

    test

--- Test/src/k.java
+++ Test/src/k.java
@@ -1,4 +1,17 @@
 
 public class k {
 
+	/*
+	 * create new repository on github
+	 * copy link
+	 * 
+	 * eclipse:
+	 * paste repository link
+	 * make new java project
+	 * team, share
+	 * select repository from git repository
+	 * done
+	 * 
+	 * easy peasy
+	 */
 }
