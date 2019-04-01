ab0627f31b735bf7c28d0010c1cd9fe30ca74898
VJ <VJ@LAPTOP-P9U4FI6A.fios-router.home>
2019-04-01 01:18:02 -0400

    same

---
+++ ProgrammingII/src/mar11/Notess.java
@@ -0,0 +1,39 @@
+package mar11;
+
+import mar4.Account;
+import mar4.SavingsAccount;
+
+public class Notess {
+
+	public Notess() {
+		// TODO Auto-generated constructor stub
+		
+		/*
+		 * Overload: methods with same name, but diff parameters in the same class
+		 * Override: change method contents of method in base class
+		 */
+		
+		
+		/*
+		 * Abstrct classes:
+		 * 	CANNOT be instantiated ; cannot be called or create an object of it
+		 * 	Serves as a superclass
+		 * 	MUST HAVE abstract method 
+		 * 		must be overriden in subclass
+		 * 		no body, just header and then a semicolon ;
+		 */
+		
+		/*
+		 * Account object = accs[i];
+			if (object instanceof SavingsAccount) { //CheckingAccount
+				SavingsAccount employee = (SavingsAccount) object;
+			    //employee.setLimit(12);
+			}
+			else {
+			    System.err.println("Object is not an Employee: we can not call getSalary()!");
+			}
+		 */
+	}
+	
+
+}
