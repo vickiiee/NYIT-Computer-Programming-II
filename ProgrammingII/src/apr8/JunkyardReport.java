c0778e66148572287891c0cad8460b66b0fb0e93
VJ <VJ@LAPTOP-P9U4FI6A.fios-router.home>
2019-04-17 23:26:50 -0400

    Final Clean

--- ProgrammingII/src/apr8/JunkyardReport.java
+++ ProgrammingII/src/apr8/JunkyardReport.java
@@ -1,5 +1,8 @@
 package apr8;
 
+//Vickie Wu
+//Apr 8 2019
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -12,7 +15,7 @@ public class JunkyardReport {
 	private File file;
 	private Scanner inputFile;
 	private String filename;
-	private Scanner keyboard;
+	private Scanner keyboard; //user input for file name
 
 	private String analyze; // A line from file
 
@@ -29,8 +32,7 @@ public class JunkyardReport {
 	/*
 	 * AUDI: 0 BMW: 1 JAGUAR: 2 LEXUS: 3
 	 */
-	
-	
+
 	private int audi;
 	private int bmw;
 	private int jaguar;
@@ -39,106 +41,11 @@ public class JunkyardReport {
 
 	public JunkyardReport() throws IOException {
 		ppl = new ArrayList<Salesperson>();
-		//cars = new int[4];
 		printReport();
 	}
 
 	public static void main(String[] args) throws IOException {
-
 		new JunkyardReport();
-		/*
-		 * JunkyardReport jk = new JunkyardReport(); keyboard = new Scanner(System.in);
-		 * System.out.print("Enter the filename: "); // src/apr8/Junkyard.txt
-		 * 
-		 * jk.openFile();
-		 * 
-		 * System.out.println("Welcome to Luxury Junkyard, Inc. the end of month report"
-		 * );
-		 * System.out.println("--------------------------------------------------------"
-		 * );
-		 * 
-		 * System.out.println("CURRENT SELLING PRICES");
-		 * System.out.println("----------------------");
-		 * 
-		 * System.out.printf("%-10.30s  %-30.30s%n", "AUDI", "8876.50"); // %-30.30s
-		 * System.out.printf("%-10.30s  %-30.30s%n", "BMW", "9909.89");
-		 * System.out.printf("%-10.30s  %-30.30s%n", "JAGUAR", "10999.99");
-		 * System.out.printf("%-10.30s  %-30.30s%n", "LEXUS", "7776.69");
-		 * System.out.println(
-		 * "\n------------------------------------------------------------------");
-		 * 
-		 * System.out.printf("%-15.30s  %-15.30s  %-15.30s  %-15.30s%n", "SALESPERSON",
-		 * "MODEL", "COUNT", "TOTAL SOLD $");
-		 * System.out.printf("%-15.30s  %-15.30s  %-15.30s  %-15.30s%n", "ID",
-		 * "DESCRIPTION", "SOLD", "PER MODEL"); System.out.println(
-		 * "------------------------------------------------------------------");
-		 * 
-		 * //getData(); /*
-		 * 
-		 * /*while (inputFile.hasNext()) {
-		 * 
-		 * analyze = inputFile.nextLine(); Scanner x = new Scanner(analyze);
-		 * 
-		 * // zybooks!!!! int id = x.nextInt(); int numOfSales = x.nextInt(); int
-		 * modelCode = x.nextInt(); int numSold = x.nextInt();
-		 * 
-		 * // System.out.println("...." + id + "...." +numOfSales +"...." + modelCode //
-		 * +"...." + numSold);
-		 * System.out.printf("%-15.30s  %-15.30s  %-15.30s  %-15.30s%n", id, numOfSales,
-		 * modelCode, numSold);
-		 * 
-		 * if (modelCode == 1) { cars[0] = numSold; } else if (modelCode == 2) { cars[1]
-		 * = numSold; } else if (modelCode == 3) { cars[2] = numSold; } else if
-		 * (modelCode == 4) { cars[3] = numSold; }
-		 * 
-		 * numOfSales--; while (numOfSales > 0) { analyze = inputFile.nextLine();
-		 * Scanner tes = new Scanner(analyze);
-		 * 
-		 * modelCode = tes.nextInt(); numSold = tes.nextInt();
-		 * 
-		 * if (modelCode == 1) { cars[0] = numSold; } else if (modelCode == 2) { cars[1]
-		 * = numSold; } else if (modelCode == 3) { cars[2] = numSold; } else if
-		 * (modelCode == 4) { cars[3] = numSold; } numOfSales--;
-		 * 
-		 * // System.out.println("...." + id + "...." +numOfSales +"...." + modelCode //
-		 * +"...." + numSold);
-		 * System.out.printf("%-15.30s  %-15.30s  %-15.30s  %-15.30s%n", " ", " ",
-		 * modelCode, numSold);
-		 * 
-		 * }
-		 * 
-		 * // for(int i = 0; i< cars.length;i++) { // System.out.print("Index: "+ i+
-		 * "Total: "+cars[i] + "\n"); // cars[i]=0; // }
-		 * 
-		 * ppl.add(new Salesperson(id, cars));
-		 * 
-		 * } int tot = 0; for (int i = 0; i < ppl.size(); i++) { tot++; }
-		 * System.out.print(tot); /* String data = inputFile.nextLine(); String[] pieces
-		 * = data.split(" "); for(int i = 0; i<pieces.length; i++) {
-		 * 
-		 * System.out.println(pieces[i]);
-		 * 
-		 * }
-		 */
-
-		// System.out.println(analyze);
-
-		/*
-		 * for(int i = 0; i<analyze.length(); i++) {
-		 * if(!analyze.substring(i,i+1).equals(" ")) {
-		 * 
-		 * System.out.println(analyze.substring(i,i+1)); } }
-		 */
-
-		/*
-		 * System.out.println("\n"); // Read lines from the file until no more are left.
-		 * while (inputFile.hasNext()) { // Read the next name. String friendName =
-		 * inputFile.nextLine();
-		 * 
-		 * // Display the last name read. System.out.println(friendName); }
-		 * 
-		 * // Close the file. inputFile.close();
-		 */
 	}
 
 	public void printReport() throws IOException {
@@ -165,28 +72,23 @@ public class JunkyardReport {
 		System.out.println("------------------------------------------------------------------");
 
 		getData();
-		
-		for(int i = 0; i <ppl.size(); i++) {
-			audi+= ppl.get(i).getAudi();
+
+		for (int i = 0; i < ppl.size(); i++) {
+			audi += ppl.get(i).getAudi();
 			bmw += ppl.get(i).getBmw();
 			jaguar += ppl.get(i).getJaguar();
 			lexus += ppl.get(i).getLexus();
-			totalSales+=ppl.get(i).getTotalSales();
+			totalSales += ppl.get(i).getTotalSales();
 		}
-		
-		System.out.println("OVERALL AUDI   UNITS SOLD: ");
-		System.out.println(audi);
-		System.out.println("OVERALL BMW    UNITS SOLD:");
-		System.out.println(bmw);
-		System.out.println("OVERALL JAGUAR UNITS SOLD:");
-		System.out.println(jaguar);
-		System.out.println("OVERALL LEXUS  UNITS SOLD:");
-		System.out.println(lexus +"\n");
-		
-		System.out.println("NUMBER SALESPERSONS: "+ppl.size());
-		//System.out.println();
-		System.out.println("OVERALL SOLD:                 $"+String.format("%.2f", totalSales));
-		System.out.println("AVERAGE SOLD per SALESPERSON: $"+String.format("%.2f", (totalSales/12)));
+
+		System.out.println("OVERALL AUDI   UNITS SOLD: " + audi);
+		System.out.println("OVERALL BMW    UNITS SOLD: " + bmw);
+		System.out.println("OVERALL JAGUAR UNITS SOLD: " + jaguar);
+		System.out.println("OVERALL LEXUS  UNITS SOLD: " + lexus + "\n");
+
+		System.out.println("NUMBER SALESPERSONS: " + ppl.size());
+		System.out.println("OVERALL SOLD:                 $" + String.format("%.2f", totalSales));
+		System.out.println("AVERAGE SOLD per SALESPERSON: $" + String.format("%.2f", (totalSales / 12)));
 	}
 
 	public void getData() {
@@ -200,10 +102,6 @@ public class JunkyardReport {
 			numOfSales = fileLine.nextInt();
 			modelCode = fileLine.nextInt();
 			numSold = fileLine.nextInt();
-
-			// System.out.println("...." + id + "...." +numOfSales +"...." + modelCode
-			// +"...." + numSold);
-//System.out.printf("%-15.30s  %-15.30s  %-15.30s  %-15.30s%n", id, numOfSales, modelCode, numSold);
 
 			if (modelCode == 1) {
 				cars[0] = numSold;
@@ -234,40 +132,13 @@ public class JunkyardReport {
 					cars[3] = numSold;
 				}
 				numOfSales--;
-
-				// System.out.println("...." + id + "...." +numOfSales +"...." + modelCode
-				// +"...." + numSold);
-//System.out.printf("%-15.30s  %-15.30s  %-15.30s  %-15.30s%n", " ", " ", modelCode, numSold);
 			}
-
-			
-			
-ppl.add(new Salesperson(id, cars));
-			
+			ppl.add(new Salesperson(id, cars));
 		}
-		
-		/*for (int i = 0; i < ppl.size(); i++) {
-				// cars[i] = 0;
-				//System.out.print("\nARRRRIndex:------------------------------------------------");
-
-				// = new int[4];
-				int [] w = ppl.get(i).getQcars();
-				for (int h = 0; h < w.length; h++) {
-					System.out.print("Index: " + h + "Total: " + w[h] + "\n");
-
-				}
-				
-			}*/
 
 		for (int i = 0; i < ppl.size(); i++) {
 			ppl.get(i).sales();
-		//	System.out
 		}
-		System.out.print(ppl.size());
-//for (int i = 0; i < cars.length; i++) {
-//				System.out.print("Index: " + i + "Total: " + cars[i] + "\n");
-
-	//		}
 
 		inputFile.close();
 	}
@@ -277,12 +148,11 @@ ppl.add(new Salesperson(id, cars));
 		filename = keyboard.nextLine();
 
 		try {
-			file = new File(filename); // Freaking src,
+			file = new File(filename); // src/apr8/Junkyard.txt
 			inputFile = new Scanner(file);
 
 			System.out.println("\nFile Found!\n");
 		} catch (FileNotFoundException e) {
-
 			System.out.print("File not found, please enter the file name:");
 			openFile();
 		}
--- ProgrammingII/src/apr8/Salesperson.java
+++ ProgrammingII/src/apr8/Salesperson.java
@@ -9,51 +9,21 @@ public class Salesperson {
 
 	private int carCount;
 	private int totalCars;
-	
-	private double  totalSales;
-	
-	private int audi;
-	private int bmw;
-	private int jaguar; 
-	private int lexus;
+
+	private double totalSales;
+
 	/*
 	 * AUDI: 0 BMW: 1 JAGUAR: 2 LEXUS: 3
 	 */
 
-	/*
-	 * private String carCosts; private String carModel; private String
-	 * numCarSold;
-	 */
 	public Salesperson(int id, int[] cars) {
 		this.id = id;
-		//cars = new int [4];
 		this.qcars = cars;
-		
-		
 	}
-
-	/*
-	 * public Salesperson(String id, String carCosts, String carModel, String
-	 * numCarSold) { this.id = id; this.carCosts = carCosts; this.carModel =
-	 * carModel; this.numCarSold = numCarSold; }
-	 * 
-	 * public Salesperson(String id, String carCosts, String carModel, String
-	 * carModel1,String numCarSold) { this.id = id; this.carCosts = carCosts;
-	 * this.carModel = carModel; this.numCarSold = numCarSold; }
-	 * 
-	 * public Salesperson(String id, String carCosts, String carModel, String
-	 * carModel1,String carModel2,String numCarSold) { this.id = id; this.carCosts
-	 * = carCosts; this.carModel = carModel; this.numCarSold = numCarSold; }
-	 * 
-	 * public Salesperson(String id, String carCosts, String carModel,String
-	 * carModel1, String carModel2,String carModel3, String numCarSold) { this.id =
-	 * id; this.carCosts = carCosts; this.carModel = carModel; this.numCarSold =
-	 * numCarSold; }
-	 */
 
 	public void sales() {
 		boolean first = true;
-		//System.out.println("begin:     a: " + qcars[0] + "b: "+qcars[1] + "j: "+qcars[2] + "l: "+qcars[3]);
+		//look through array and figure out how many are audi, bmw, jaguar, lexus
 		for (int i = 0; i < qcars.length; i++) {
 			if (qcars[i] != 0) {
 				if (i == 0) {
@@ -74,6 +44,7 @@ public class Salesperson {
 					carCount = qcars[i];
 				}
 
+				//if its the first time printing, print out id as well
 				if (first) {
 					first = false;
 					System.out.printf("%-15.30s  %-15.30s  %-15.30s  %10.30s%n", id, carModel, carCount, String.format("%.2f", calculate(i)));
@@ -84,24 +55,11 @@ public class Salesperson {
 
 		}
 		System.out.println("                            ---------------------------------------");
-		//System.out.printf("%-15.30s  %-15.30s  %-15.30s  %10.30s%n", "",  "------------------------------------", "----------------------------------------------", "");
+
 		System.out.printf("%-15.30s  %-15.30s  %-15.30s  %10.30s%n", "", "", totalCars, String.format("%.2f", totalSales));
 		System.out.print("\n");
-
-		
-		// System.out.printf("%-15.30s %-15.30s %-15.30s %-15.30s%n", "ID",
-		// "DESCRIPTION", "SOLD", "PER MODEL");
-
 	}
 
-	/*
-	 * AUDI: 0 BMW: 1 JAGUAR: 2 LEXUS: 3
-	 */
-
-//	System.out.printf("%-10.30s  %-30.30s%n", "AUDI", "8876.50"); // %-30.30s
-	// System.out.printf("%-10.30s %-30.30s%n", "BMW", "9909.89");
-//	System.out.printf("%-10.30s  %-30.30s%n", "JAGUAR", "10999.99");
-	// System.out.printf("%-10.30s %-30.30s%n", "LEXUS", "7776.69");
 	public double calculate(int modelCode) {
 		if (modelCode == 0) {
 			totalSales += qcars[modelCode] * (8876.50);
@@ -140,6 +98,5 @@ public class Salesperson {
 	public int getLexus() {
 		return qcars[3];
 	}
-
 
 }
