a3d778010f31e8dca32a1e09f3c8319eb89ff743
VJ <VJ@216.37.99.100>
2019-04-22 19:32:23 -0400

    Given code

---
+++ ProgrammingII/src/apr22/LinkStackSApp.java
@@ -0,0 +1,24 @@
+package apr22;
+
+class LinkStackSApp {
+	public static void main(String[] args) {
+		LinkStackS theStack = new LinkStackS(); // make stack
+
+		theStack.push("20 twenty"); // push items
+		theStack.push("40 fourty");
+
+		theStack.displayStack(); // display stack
+
+		theStack.push("60 sixty"); // push items
+		theStack.push("80 eighty");
+
+		theStack.displayStack(); // display stack
+
+		theStack.pop(); // pop items
+		theStack.pop();
+
+		theStack.displayStack(); // display stack
+
+	} // end main()
+} // end class LinkStackApp
+////////////////////////////////////////////////////////////////
